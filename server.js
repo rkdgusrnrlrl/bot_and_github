12a1bc38cbb9a9e78239f0fa64a883af0a3d31ad
강현구 <rkdgusrnrlrl@gmail.com>
2017-08-19 21:18:57 +0900

    바디 파서 추가함

--- server.js
+++ server.js
@@ -3,15 +3,19 @@
  */
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-app.get('/github/build/bot', function (req, res) {
+app.post('/github/build/bot', function (req, res) {
+    console.log(req.body);
     res.send('Hello World!');
 });
-
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
